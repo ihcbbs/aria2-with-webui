f31bdda01df0eac8c3944ab62cdf381832831929
ihcbbs <doruison@gmail.com>
2018-06-20 15:27:29 +0800

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -17,5 +17,5 @@ fi
 chmod +x /conf/on-complete.sh
 touch /conf/aria2.session
 
-darkhttpd /aria2-webui --port 80 &
-darkhttpd /data --port 8080 
+darkhttpd /aria2-webui --port 680 &
+darkhttpd /data --port 6880 
